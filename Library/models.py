d42de33cac6145b9fc35f620f781b8396607a806
AndriiMikita <103601417+AndriiMikita@users.noreply.github.com>
2023-05-31 23:56:39 +0300

    7

--- Library/models.py
+++ Library/models.py
@@ -22,11 +22,11 @@ class Genre(models.Model):
 
  
 class Book(models.Model): 
-    title = models.CharField(max_length=200, verbose_name='Назва', unique=True) 
-    authors = models.ManyToManyField(Author, verbose_name='Автори') 
-    tags = models.ManyToManyField(Tag, verbose_name='Теги') 
-    genres = models.ManyToManyField(Genre, verbose_name='Жанри') 
-    coverart = models.ImageField(upload_to="images/", default="", verbose_name='Зображення')
+    title = models.CharField(max_length=200, verbose_name='Назва', unique=True, on_delete=models.CASCADE) 
+    authors = models.ManyToManyField(Author, verbose_name='Автори', on_delete=models.CASCADE) 
+    tags = models.ManyToManyField(Tag, verbose_name='Теги', on_delete=models.CASCADE) 
+    genres = models.ManyToManyField(Genre, verbose_name='Жанри', on_delete=models.CASCADE) 
+    coverart = models.ImageField(upload_to="images/", default="", verbose_name='Зображення', on_delete=models.CASCADE)
     description = models.TextField(verbose_name='Опис')
     
     def __str__(self):
